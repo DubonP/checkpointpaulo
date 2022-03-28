654c6537b64978998f66489b77c3c6a098c3a57b
PauloRossi <paulogustavorossi@gmail.com>
2022-03-28 19:29:58 -0300

    footer finalizado

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Navbar, Nav } from "react-bootstrap";
-import { ArrowRight } from "react-bootstrap-icons";
+import { Whatsapp, Envelope, Github, Linkedin } from "react-bootstrap-icons";
 
 function Footer() {
     return (
@@ -9,35 +9,31 @@ function Footer() {
             <Navbar.Toggle />
             <Navbar.Collapse className='right-aligned'>
             <div className="container">
-                <div className="row position-relative" >
-                    
-                    <div className="col-md-3 col-sm-6">
-                        <h4>ahsuhasuahsuaushuas</h4>
-                        <ul className="list-unstyled">
-                            <li>auhauhuahua</li>
-                            <li>auhauhuahua</li>
-                        </ul>
-                    </div>
+                <div className="row justify-content-md-center">
 
-                    <div className="col-md-3 col-sm-6">
+                    <div className="col col-lg-2">
                         <h4>Redes Sociais</h4>
                         <ul className="list-unstyled">
-                            <Nav.Link href='https://github.com/DubonP' target="_blank" id='mygit' >Git page</Nav.Link>
-                            <Nav.Link href='https://www.linkedin.com/in/paulo-rossi-95296a4b/' target="_blank" id='mylinkedin' >Linkedin</Nav.Link>
+                            <Nav.Link href='https://github.com/DubonP' target="_blank" id='mygit' ><Github /> Git page</Nav.Link>
+                            <Nav.Link href='https://www.linkedin.com/in/paulo-rossi-95296a4b/' target="_blank" id='mylinkedin' ><Linkedin /> Linkedin</Nav.Link>
                         </ul>
                     </div>
 
-                    <div className="col-md-3 col-sm-6">
+                    <div className="col col-lg-3"></div>
+
+                    <div className="col col-lg-2">
                         <h4>Contato</h4>
                         <ul className="list-unstyled">
-                            <Nav.Link href="https://wa.me/5517981350990" target="_blank" id='mygit' >Whatsapp</Nav.Link>
+                        <Nav.Link href="https://wa.me/5517981350990" target="_blank" id='mygit' ><Whatsapp /> Whatsapp</Nav.Link>
+                        <Nav.Link href="mailto:paulogustavorossi@gmail.com?" target="_blank" id='myemail' ><Envelope /> Email</Nav.Link>
                         </ul>
                     </div>
                     
                 </div>
-                {/* footer bottom */}
+
                 <div className="footer-bottom">
                     <p className="text-xs-center">
+                        Paulo Rossi
                         &copy;{new Date().getFullYear()}
                     </p>
                 </div>
